6c8762114ce5f9bffde1e2ecbdb9a015c861cb3d
Jonathan Willing <jwilling@me.com>
2012-07-27 11:39:50 -0500

    add missing license

--- JWSplitViewDemo/main.m
+++ JWSplitViewDemo/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  JWSplitViewDemo
-//
-//  Created by Jonathan Willing on 7/25/12.
-//  Copyright (c) 2012 Jonathan Willing. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
